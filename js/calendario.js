66275462c6709de8ec1acb792e62dd08d16840e1
Rosa <costurezati@gmail.com>
2024-12-06 06:37:12 -0300

    .

--- js/calendario.js
+++ js/calendario.js
@@ -1,4 +1,4 @@
-let el = document.querySelector('.data');
-for(i=0;i<=31;i++){
-    el.innerHTML+='<span>'+i+'</span>';
-}
+// let el = document.querySelector('.data');
+// for(i=0;i<=31;i++){
+//     el.innerHTML+='<span>'+i+'</span>';
+// }
